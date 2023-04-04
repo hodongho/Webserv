b28377f40e25d2fb047d3c30e8ed892db3b44586
Jake1152 <rancho974@gmail.com>
2023-04-04 20:27:25 +0900

    CH_FUNC
    modify config structure member variable data type

--- include/configStructure.hpp
+++ include/configStructure.hpp
@@ -8,27 +8,31 @@
 struct ServerConfig;
 struct LocationConfig;
 
-struct LocationConfig
-{
-	std::string					location_path;
-	std::vector<std::string>	allow_method;
-	std::string					root;
-	std::string					index;
-	bool						autoindex;
-	// std::map<int, std::string>	redirection_path; // 더 많은 값이 list도 들어오는지 확인 및 고민필요
-	/* to be added cgi infomation */
-};
-
 struct ServerConfig
 {
-	std::string					server_name;
-	std::string					ip;		// char *고려 
-	uint16_t					port;	//unsigned short
-	std::string					root;
-	std::string					index;
-	size_t						client_max_body_size;
-	std::map<int, std::string>	error_page;
-	std::vector<LocationConfig>	locations;
+	std::string								host;					// 필수 필드, IPv4 style "0.0.0.0"
+	uint16_t								port;					// 필수 필드, 0~65525 범위 내에 존재해야함
+	std::string								root;					// 필수 필드, 빈 문자열 입력은 허용하지 않음
+	std::string								index;					// 필수 필드, 빈 문자열 입력은 허용하지 않음
+	size_t									client_max_body_size;	// 필수 필드, 양수만 허용, atoi로 변환하여 처리, 숫자가 아닌 문자열인 경우는?, isdigit()사용
+	std::map<int, std::string>				default_error_page;		// config에는 없지만 error_page에 없는 status code에 대한 default error page 경로를 담고 있음, 항상 값이 있음
+	std::map<int, std::string>				error_page;				// 선택 필드, 입력값 없으면 size() == 0
+	std::string								server_name;			// 선택 필드, 입력값 없으면 ""
+	std::map<std::string, LocationConfig>	locations;				// 선택 필드, 입력값 없으면 size() == 0
+};
+
+enum Method {}; // temp
+
+struct LocationConfig
+{
+	// std::string					location_path;	// key로 이동 (std::map<std::string, LocationConfig>	locations)
+	std::map<Method, bool>		allow_method;	// 필수 필드, 입력값 없으면 default를 MethodType enum으로 초기화
+	bool						autoindex;		// 필수 필드, 입력값 없으면 default를 false -> "off"를 의미
+	std::string					root;			// 선택 필드, 입력값 없으면 ""
+	std::string					index;			// 선택 필드, 입력값 없으면 ""
+	std::string					redirect;		// 선택 필드, 입력값 없으면 "" , default로 301 code만 들어온다고 가정
+	/* to be added cgi infomation!!! */
+	//예상 std::map<std::string, CgiInfo>	cgis;
 };
 
 #endif
