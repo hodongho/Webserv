6b66e854f261c9b6776860c0cd3d87197739e706
dhyun <ehdghho@kakao.com>
2023-04-12 18:54:02 +0900

    CH_REF
    fix pages

--- var/upload.php
+++ var/upload.php
@@ -1,34 +1,14 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>CGI POST</title>
-</head>
-<body>
-<div style="text-align: center;">
 <?php
+
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    // Check if a file was uploaded
-    if (isset($_FILES['image']) && $_FILES['image']['error'] === UPLOAD_ERR_OK) {
-        // Retrieve the file information
-        $file_name = $_FILES['image']['name'];
-        $file_size = $_FILES['image']['size'];
-        $file_tmp = $_FILES['image']['tmp_name'];
-        $file_type = $_FILES['image']['type'];
-
-        // Move the uploaded file to a new location
-        $target_dir = "uploads/";
-        $target_file = $target_dir . basename($file_name);
-        move_uploaded_file($file_tmp, $target_file);
-
-        // Display a success message
-        echo "<p>The image $file_name has been uploaded.</p>";
-    } else {
-        // Display an error message
-        echo "<p>There was an error uploading the image.</p>";
-    }
+	$uploads_dir = './uploads/';
+	$filename = basename($_FILES['file']['name']);
+	$upload_path = $uploads_dir . $filename;
+	if (move_uploaded_file($_FILES['file']['tmp_name'], $upload_path)) {
+		http_response_code(200);
+	} else {
+		http_response_code(500);
+	}
 }
-?>
-</div>
-</body>
-</html>
 
+?>
